749f79fb310b0c805c7f38b4ac08a1b06eef678f
Dukunde Irakoze Happy <acele4444@gmail.com>
2025-03-19 23:28:00 +0200

    basic model design

---
+++ models/Product.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+
+const productSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required:true
+    },
+    description:{
+        type: String,
+        required:true
+    },
+    category:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Category",
+        required:true
+    },
+    price:{
+        type: String,
+        required:true
+    },
+    imgUrl:{
+        type: String,
+        required:true
+    },
+})
