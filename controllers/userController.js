86f8b0e904c6f7b21fecf30cf1ffc1ce41b14f24
Dukunde Irakoze Happy <acele4444@gmail.com>
2025-03-24 19:57:08 +0200

    usercontroller updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,14 +9,14 @@ exports.register = async (req, res) => {
     const user = req.body;
     const duplicateEmail = await User.findOne({ email: user.email });
     if (duplicateEmail) {
-      return res.send("Email already exist!");
+      return res.status(409).send({message: "Email already exist!", error: "Duplicate",data:null});
     }
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     const newUser = await new User(user).save();
     return res.status(200).send({message: "Success", data:newUser});
   } catch (err) {
-    return res.status(500).send(err);
+    return res.status(500).send({error: err.message});
   }
 };
 
@@ -24,7 +24,7 @@ exports.login = async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     if (!user) {
-      return res.status(404).send({message: "Invalid email or password!"});
+      return res.status(404).send({message: "Invalid email or password!",error:"Not found",data:null});
     }
 
     const isPasswordValid = bcrypt.compareSync(
@@ -32,7 +32,7 @@ exports.login = async (req, res) => {
       user.password
     );
     if (!isPasswordValid) {
-      return res.status(404).send({message: "Invalid email or password!"});
+      return res.status(404).send({message: "Invalid email or password!",error:"Not found",data:null});
     }
     const token = jwt.sign(
       {
@@ -69,15 +69,14 @@ exports.login = async (req, res) => {
 exports.getUserByToken = async (req, res) => {
   try {
     const token = req.headers.authorization?.split(' ')[1];
-    console.log(req.headers.authorization)
     if (!token) {
-      return res.status(401).json({message:"Access denied. No token provided."});
+      return res.status(401).json({message:"Access denied. No token provided.",error:"Access denied"});
     }
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
 
     const user = await User.findById(decoded._id);
     if (!user) {
-      return res.status(404).json({message:"User not found."});
+      return res.status(404).json({message:"User not found.",error:"Not found"});
     } 
 
     return res.status(200).json({
@@ -93,15 +92,15 @@ exports.addToCart = async (req, res) => {
   try {
     const { userId, productId, quantity } = req.body;
     if (!userId || !productId || quantity < 1) {
-      return res.status(400).json({ error: "Invalid data provided" });
+      return res.status(400).json({ error: "Bad request",message: "Invalid data provided" });
     }
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: "Not found",message: "User not found" });
     }
     const product = await Product.findById(productId);
     if (!product) {
-      return res.status(404).json({ error: "Product not found" });
+      return res.status(404).json({ error: "Not found",message:"Product not found" });
     }
     const cartItem = user.cart.find((item) => item.productId.equals(productId));
 
@@ -113,7 +112,7 @@ exports.addToCart = async (req, res) => {
     await user.save();
     res.status(200).send({message:"Added to Cart!", data:cartItem});
   } catch (err) {
-    res.status(500).send({message: err.message});
+    res.status(500).send({error: err.message});
   }
 };
 exports.removeFromCart = async (req, res) => {
@@ -121,17 +120,17 @@ exports.removeFromCart = async (req, res) => {
     const { userId, productId } = req.body;
 
     if (!userId || !productId) {
-      return res.status(400).json({ error: "Invalid data provided" });
+      return res.status(400).json({ error: "Bad request",message:"Invalid data provided" });
     }
 
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: "Not found",message:"User not found" });
     }
     const cartItemIndex = user.cart.findIndex((item) => item.productId.equals(productId));
 
     if (cartItemIndex === -1) {
-      return res.status(400).json({ error: "Product not in cart" });
+      return res.status(404).json({ error: "Not found",message:"Product not in cart" });
     }
     user.cart.splice(cartItemIndex, 1);
     await user.save();
@@ -141,7 +140,7 @@ exports.removeFromCart = async (req, res) => {
       data: user.cart,
     });
   } catch (error) {
-    return res.status(500).send({message: error.message});
+    return res.status(500).send({error: error.message});
   }
 };
 
@@ -149,15 +148,15 @@ exports.addToWishlist = async (req, res) => {
   try {
     const { userId, productId } = req.body;
     if (!userId || !productId) {
-      return res.status(400).json({ error: "Invalid data provided" });
+      return res.status(400).json({ error: "Bad request",message:"Invalid data provided" });
     }
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: "Not found",message:"User not found" });
     }
     const product = await Product.findById(productId);
     if (!product) {
-      return res.status(404).json({ error: "Product not found" });
+      return res.status(404).json({ error: "Not found", message:"Product not found" });
     }
 
     if (!user.wishlist.includes(productId)) {
@@ -171,25 +170,25 @@ exports.addToWishlist = async (req, res) => {
         });
     }
 
-    return res.status(200).send({message: "Product is already in wishlist"});
+    return res.status(409).send({message: "Product is already in wishlist",error:"Duplicate"});
   } catch (error) {
-    return res.status(500).send({message: error.message});
+    return res.status(500).send({error: error.message});
   }
 };
 exports.removeFromWishlist = async (req, res) => {
   try {
     const { userId, productId } = req.body;
     if (!userId || !productId) {
-      return res.status(400).json({ error: "Invalid data provided" });
+      return res.status(400).json({ error: "Bad request", message:"Invalid data provided" });
     }
 
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: "Not found", message:"User not found" });
     }
 
     if (!user.wishlist.includes(productId)) {
-      return res.status(400).json({ error: "Product not in wishlist" });
+      return res.status(400).json({ error: "Bad request",message:"Product not in wishlist" });
     }
 
     user.wishlist = user.wishlist.filter((id) => id.toString() !== productId);
@@ -200,6 +199,6 @@ exports.removeFromWishlist = async (req, res) => {
       data: user.wishlist,
     });
   } catch (error) {
-    return res.status(500).send({message: error.message});
+    return res.status(500).send({error: error.message});
   }
 };
